2cf061ee0acbe17465cbcdbc6316d1cfb3f25543
Frank Denis <github@pureftpd.org>
2023-05-05 11:49:36 +0200

    Upgrade autoconf script

--- src/blogbench.h
+++ src/blogbench.h
@@ -9,43 +9,22 @@
 #endif
 
 #include <stdio.h>
-#ifdef STDC_HEADERS
-# include <stdlib.h>
-# include <stddef.h>
-# include <stdarg.h>
-#else
-# if HAVE_STDLIB_H
-#  include <stdlib.h>
-# endif
-#endif
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
 #ifdef HAVE_LOCALE_H
 # include <locale.h>
 #endif
-#ifdef HAVE_STRING_H
-# if !STDC_HEADERS && HAVE_MEMORY_H
-#  include <memory.h>
-# endif
-# include <string.h>
-#else
-# if HAVE_STRINGS_H
-#  include <strings.h>
-# endif
-#endif
+#include <string.h>
 #include <limits.h>
 #include <errno.h>
 #include <signal.h>
 #ifdef HAVE_UNISTD_H
 # include <unistd.h>
 #endif
-#ifdef TIME_WITH_SYS_TIME
+#include <time.h>
+#if HAVE_SYS_TIME_H
 # include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
 #endif
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -149,10 +128,10 @@ the server may be insecure if a wrong value is set here.
 Your platform has a very large MAXPATHLEN, we should not trust it.
 #endif
 
-#define MAX_OUTPUT_LINE 4096U    
-    
-int process(void);    
-    
+#define MAX_OUTPUT_LINE 4096U
+
+int process(void);
+
 unsigned long long get_new_blog_id(void);
 unsigned long long get_last_blog_id(void);
 unsigned long long get_random_blog_id(void);
